b882ceb6fe47ef6345b92f5d208db11153f987ab
Mihoko <melina.bouchelleghem@edu.univ-fcomte.fr>
2023-10-02 14:48:58 +0200

    fix(mybackgroundscroll) : composition api (fix)

--- src/components/MyBackgroundScroll.vue
+++ src/components/MyBackgroundScroll.vue
@@ -1,32 +1,32 @@
-<script>
-export default {
-    data() {
-        return {
-            percent : 0
-        }
-    },
-    methods: {
-        handleScroll (){
+<script setup>
 
-            let scrollTop = window.scrollY
-            let scrollBottom = scrollTop + window.innerHeight
-        
+import {ref, onMounted, onUnmounted }from 'vue'
 
-            let offsetTop = this.$refs.container.offsetTop
-            let offsetHeight = this.$refs.container.offsetHeight
 
-            let percent = (((scrollBottom - offsetTop) / offsetHeight) * 100)
-            this.percent = Math.round(percent)
-        
-        }
-    },
-    mounted () {
-        window.addEventListener('scroll', this.handleScroll)
-    },
-    unmounted() {
-        window.removeEventListener('scroll', this.handleScroll)
-    },
+const percent = ref(0)
+const container = ref(0)
+
+
+function handleScroll(){
+    let scrollTop = window.scrollY
+    let scrollBottom = scrollTop + window.innerHeight
+
+    let offsetTop = container.value.offsetTop
+    let elementHeight = container.value.offsetHeight
+          
+    let result = ((scrollBottom - offsetTop)/ elementHeight) * 100;
+
+    percent.value = result  > 100 ? 100 : Math.round(result)
 }
+
+
+    onMounted(() =>{
+        window.addEventListener('scroll', handleScroll)
+    })
+    onUnmounted(()=>{
+        window.removeEventListener('scroll', handleScroll)
+    })
+
 </script>
 
 
@@ -39,15 +39,15 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-    .backgroundScroll {
-        background: black;
-        height: 2000px;
-        color: $white;
-        &__content {
-            position:sticky;
-            top:0;
-            left:0;
-            width: 100%;
-        }
+.backgroundScroll{
+    background: $black;
+    color: $white;
+    height: 2000px;
+    &__content{
+        position: sticky;
+        top: 0;
+        left: 0;
+        width: 100%;
     }
+}
 </style>
