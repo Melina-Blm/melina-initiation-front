653340d048c32efd09a4f274160e6a65c0c3f8bd
Mihoko <melina.bouchelleghem@edu.univ-fcomte.fr>
2023-11-09 15:06:35 +0100

    feat(cards): modification border radius + ajout titre

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -86,6 +86,7 @@ onMounted (async () => {
     &__right {
       background-color: $white;
       border : 1px solid $gray;
+      border-radius: 20px;
       padding:rem(50);
       display: flex;
       flex-direction: column;
